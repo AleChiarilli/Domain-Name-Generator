3bc2dcd09f8a15855020cd3d4e80f331ad6ffc43
AleChiarilli <alejandrochiarilli@gmail.com>
2023-03-27 18:35:41 +0000

    primer push

---
+++ app.js
@@ -0,0 +1,14 @@
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var end = [".com"];
+
+console.log(length.pronoun);
+
+// for (let i = 0; i < pronoun.length; i++) {
+//     for (let j = 0; j < adj.length; j++) {
+//         for (let k = 0; k < noun.length; k++)
+//     }
+// }
+
+// console.log (i (j(k)));
