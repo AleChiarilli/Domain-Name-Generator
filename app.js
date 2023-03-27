0e4625f0c47a23a2af9c1e3ef47962a2136ac003
AleChiarilli <alejandrochiarilli@gmail.com>
2023-03-27 19:39:24 +0000

    segundo commit

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+// function generadorNombreDominio (pronoun, adj, noun, end) = {
+//     const = nombreDominio []
+
 var pronoun = ["the", "our"];
 var adj = ["great", "big"];
 var noun = ["jogger", "racoon"];
 var end = [".com"];
 
-console.log(length.pronoun);
+// console.log(length.pronoun);
 
-// for (let i = 0; i < pronoun.length; i++) {
-//     for (let j = 0; j < adj.length; j++) {
-//         for (let k = 0; k < noun.length; k++)
-//     }
+for (let posicion = 0; posicion < pronoun.length; posicion++) {
+  for (let posicion2 = 0; posicion2 < adj.length; posicion2++) {
+    for (let posicion3 = 0; posicion3 < noun.length; posicion3++) {
+      //     for (end)
+      //    const combinacion = pronoun(i) + adj(j) + noun(k) + end();
+      //    nombreDominio.push(combinacion);
+      console.log(
+        pronoun[posicion] + adj[posicion2] + noun[posicion3] + end[0]
+      );
+    }
+  }
+}
 // }
 
-// console.log (i (j(k)));
+// console.log (combinacion)
